599dc7e786d44c93b2fea00ccba1f32ecf6f4111
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-06-09 18:25:30 -0500

    All Base Starter Plugin Core

---
+++ src/main/kotlin/coquita/proxy/asd/Asd.kt
@@ -0,0 +1,14 @@
+package coquita.proxy.asd
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Asd : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
